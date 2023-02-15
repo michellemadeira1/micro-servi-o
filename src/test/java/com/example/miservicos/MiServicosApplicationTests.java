66174cfaff1837330398e87fbf99faf6ca40ab0e
Michelle Madeira <michellemadeira1@gmail.com>
2023-02-15 12:25:40 -0300

    criando discovery

---
+++ src/test/java/com/example/miservicos/MiServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
